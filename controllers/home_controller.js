c677e68af176e6127c950a180744582fa6f55b9d
Abhay Ranjan <abhayranjan471@gmail.com>
2022-09-20 18:38:33 +0530

    Linking all the Users to the home page , so that as we click on the particular user it will redirect us to that user profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 const { populate } = require('../models/comment')
 const Post = require('../models/post')
+const User = require('../models/user')
 
 module.exports.home = function(req, res){
      
@@ -36,10 +37,14 @@ module.exports.home = function(req, res){
     })
     .exec(function(err, posts){
         // console.log(posts);
-        return res.render('home',{
-            title: 'Codeial | Home',
-            posts: posts
-        })
+         User.find({}, function(err, users){
+            return res.render('home',{
+                title: 'Codeial | Home',
+                posts: posts,
+                all_users: users
+            })
+         })
+        
     })
 }
 
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const usersController = require('../controllers/users_controller');
                     
                      //middleware function whcih we created in passprot-local-strategy.js
-router.get('/profile',passport.checkAuthentication ,usersController.profile);
+router.get('/profile/:id',passport.checkAuthentication ,usersController.profile);
 
 router.get('/sign-up',usersController.signUp);
 
