9268b7a2340e3b85a49c8020ee3aa09f087ddd9e
Abhay Ranjan <abhayranjan471@gmail.com>
2022-09-22 16:42:02 +0530

    Converrting some functions of post_controller and commment_controller to Async Await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,7 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
+
+    /*
     //1st find the post by the post id and then create the comment, if there is no post we cannot create a comment
     Post.findById(req.body.post, function(err, post){
         //if we found the post, now create the comment
@@ -26,10 +28,39 @@ module.exports.create = function(req, res){
             })
         }
     })
+    */
+
+    ////// Converting to Aysnc Await ////////
+    try{
+        //1st find the post by the post id and then create the comment, if there is no post we cannot create a comment
+        let post = await Post.findById(req.body.post);
+
+        //if we found the post, now create the comment
+        if(post){
+           let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+
+                //when the comments get created we will add comments to the post
+                // PostSchema contains the filed comment , which is of type error
+                post.comments.push(comment);
+                //whenever we are updating something after that we need to call the save()
+                post.save();//after calling this it gets save in the dataBase
+
+                res.redirect('/');
+        }
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
 //for DELETING the Comments
-module.exports.destroy = function(req, res){
+module.exports.destroy = async function(req, res){
+
+    /*
     Comment.findById(req.params.id , function(err , comment){
         if(comment.user == req.user.id){
 
@@ -50,4 +81,29 @@ module.exports.destroy = function(req, res){
             return res.redirect('back');
         }
     })
+    */
+
+    ///// Converting it to Aysnc Await //////////
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if(comment.user == req.user.id){
+            //we will store the Id of the post which is present in the Comment Schema, So that we can delete the comment from the Post Schema also
+            //suppose if we deleted the comment without storing the id of post , to fir Post Scehma me jo humne comments array banaya hai usme jo comment hai 
+            //use nai hata paye ge .
+            let postId = comment.post;
+
+            comment.remove(); //deleting the comment
+
+            let post = await  Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});
+            return res.redirect('back');
+        }
+        else{
+            return res.redirect('back');
+        }
+        
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,7 +1,9 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
+
+    /*
     //creating a new Post
     //saving the data coming from the 'form' into the data base
    Post.create ({
@@ -14,11 +16,30 @@ module.exports.create = function(req, res){
     }
     return res.redirect('back');
    });
+   */
+
+   /// Converting to Async Await
+   try{
+    //creating a new Post
+    //saving the data coming from the 'form' into the data base
+    //will wait till the post get created
+       await Post.create ({
+            content: req.body.content,
+            user: req.user._id // we just want to store the user id nd not whole user so we did this
+       });
+
+       return res.redirect('back');
+   }
+   catch(err){
+      console.log('Error', err);
+      return;
+   }
 }
 
 // for DELETING the post
-module.exports.destroy = function(req, res){
+module.exports.destroy = async function(req, res){
     
+    /*
      //before deleting a post we need to find weither it exist or not
      Post.findById(req.params.id , function(err, post){
         //Check for autherasition, so that the user who has posted it is only allowed to delete it
@@ -35,5 +56,30 @@ module.exports.destroy = function(req, res){
             //when the user didn't matched
             return res.redirect('back');
         }
-     })
+     });
+     */
+
+     
+    ///// Converting to Async Await  /////
+    try{
+         //before deleting a post we need to find weither it exist or not
+        let post = await  Post.findById(req.params.id);
+
+         //Check for autherasition, so that the user who has posted it is only allowed to delete it
+        // .id means converting the object id into String
+        if(post.user == req.user.id){
+            post.remove();
+            //we will delete all the comment related to that post and if suppose there is an error we will return back
+            await Comment.deleteMany({post: req.params.id});
+            
+            return res.redirect('back');
+        }
+        else{
+            //when the user didn't matched
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
